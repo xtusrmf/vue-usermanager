1eb7ec6607e50f96798feb702e7074425665cb57
zhangxincai <xtusrmf@126.com>
2019-09-23 09:20:06 +0800

    主要涉及到Vue-Router的使用

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	  <h2 class="maintile">我是APP组件 的入口</h2>
+	<!-- replace 属性是让用户不能在网页中点返回就跳转到上一个页面，必须通过相应的点击才能进行跳转 -->
+	
+<!-- 	<router-link to="/home" tag="button" replace>首页</router-link>&nbsp;
+	<router-link to="/user" tag="button" replace>用户</router-link>&nbsp;
+	<router-link to="/about" tag="button" replace>关于</router-link>&nbsp; -->
+<!-- 	<button @click="homeClick">首页</button>
+	<button @click="userClick">用户</button>
+	<button @click="aboutClick">关于</button> -->
+	<router-link to="/home" tag="button" replace>首页</router-link>&nbsp;
+	<router-link v-bind:to="'/user/'+userName" tag="button" replace>用户</router-link>&nbsp;
+	<router-link to="/about" tag="button" replace>关于</router-link>&nbsp;
+	<router-link v-bind:to="{path:'/profile',query:{name:'zhangxincai',age:30,city:'BJ'}}" tag="button" replace>档案</router-link>&nbsp;
+    <keep-alive exclude="Profile,User">
+		<router-view></router-view>
+	</keep-alive>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // }
+  data(){
+	  return{
+		  userName:"zhangxincai"
+	  }
+  },
+  methods:{
+	  homeClick(){
+		  // 通过代码修改路由
+		  this.$router.push("/home")
+	  },
+	  userClick(){
+		  this.$router.push("/user")
+	  },
+	  aboutClick(){
+	  	   this.$router.push("/about")
+	  }
+  },
+  created() {
+  	// console.log(this.$router)
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.router-link-active{
+	background: #42B983;
+}
+
+.maintile{
+	background: greenyellow;
+	margin: 10px auto;
+	width:300px;
+	height: 50px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<h3>首页</h3>
+		<router-link to="/home/news" tag="button">新闻</router-link>
+		<!-- <router-link to="/home/message" tag="button">消息</router-link> -->
+		<button @click="msgClick">消息</button>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+export default{
+	name:"Home",
+	methods:{
+		msgClick(){
+			this.$router.push({path:"/home/message",query:{country:"China"}})
+		}
+	},
+	created(){
+		console.log("home created")
+	},
+	destroyed() {
+		console.log("home destroyed")
+	}
+}
+</script>
+
+<style>
+.router-link-active{
+	background:#32CD32
+}
+</style>
